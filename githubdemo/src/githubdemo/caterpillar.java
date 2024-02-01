2370f4983a911a1c752e010c317715c1bf951d26
Siddi Nalgonda <srinivasnolgonda@gmail.com>
2024-02-01 10:24:07 +0530

    commit

---
+++ githubdemo/src/githubdemo/caterpillar.java
@@ -0,0 +1,14 @@
+package githubdemo;
+
+public class caterpillar {
+
+int moves,cabbages;
+
+caterpillar(int moves ,int cabbages) {
+	
+    this.moves = moves;
+     this.cabbages= cabbages;
+	}
+
+}
+
